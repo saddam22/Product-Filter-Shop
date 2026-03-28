df8e6c30095f5270463c62b5fbedbd516a0182e9
Md.Saddam Hossain <csesolve6548@gmail.com>
2026-03-28 19:04:54 +0600

    Update Basic Product Filter Shop
    Category filter
    Price range filter (with dynamic label)
    Rating filter
    Responsive grid with Tailwind
    Hover animations for product cards

---
+++ products.js
@@ -0,0 +1,38 @@
+const products = [
+{
+id: 1,
+name: "Wireless Headphones",
+category: "Electronics",
+price: 120,
+rating: 4.5,
+color: "Black",
+image: "assets/images/headphone.jpg"   
+},
+{
+id: 2,
+name: "Men's Casual Shirt",
+category: "Fashion",
+price: 35,
+rating: 4.2,
+color: "Blue",
+image: "assets/images/shirt.jpg"   
+},
+{
+id: 3,
+name: "Coffee Maker",
+category: "Home",
+price: 80,
+rating: 4.8,
+color: "Red",
+image: "assets/images/coffeemaker.jpg"   
+},
+{
+id: 4,
+name: "Toy Car",
+category: "Toys",
+price: 15,
+rating: 4.0,
+color: "Yellow",
+image: "assets/images/toycar.jpg"   
+}
+];
---
+++ script.js
@@ -0,0 +1,43 @@
+const categoryFilter = document.getElementById("categoryFilter");
+const priceFilter = document.getElementById("priceFilter");
+const ratingFilter = document.getElementById("ratingFilter");
+const priceValue = document.getElementById("priceValue");
+const productContainer = document.getElementById("productContainer");
+
+//Update price Label
+priceFilter.addEventListener("input", () =>{
+    priceValue.textContent = priceFilter.value;
+    filterProducts();
+});
+
+categoryFilter.addEventListener("change", filterProducts);
+ratingFilter.addEventListener("change", filterProducts);
+
+// Function to display products
+function displayProducts(items){
+    productContainer.innerHTML = items.map(p => `
+        <div class="bg-white p-4 rounded shadow hover:shadow-lg transition">
+        <img src="${p.image}" alt="${p.name}" class="w-full h-40 object-cover mb-2 rounded" />
+        <h3 class="font-semibold">${p.name}</h3>
+        <p class="text-gray-600">${p.price}</p>
+        <p class="text-yellow-500">${'★'.repeat(Math.floor(p.rating))}</p>
+        </div>
+        `).join("");
+}
+
+// Filter Function
+function filterProducts(){
+    const category = categoryFilter.value;
+    const maxPrice = Number(priceFilter.value);
+    const minRating = Number(ratingFilter.value);
+
+    const filtered = products.filter(p => {
+        return (category === "all" || p.category ===category) && 
+        p.price <= maxPrice &&
+        p.rating >= minRating;
+    });
+    displayProducts(filtered);
+}
+
+//Initial load
+displayProducts(products);
